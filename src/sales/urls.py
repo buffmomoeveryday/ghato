cd92c3e8e64f7ffb8b8fed6cdaada122569ac7da
Siddhartha Khanal <sidkhanal77@gmail.com>
2024-07-25 23:49:23 +0545

    adding payments urls

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import sales_add, sales_all, sales_detail
+from .views import sales_add, sales_all, sales_detail, payments_received
 
 sales = [
     path("sales/all/", sales_all, name="sales_list"),
@@ -8,7 +8,14 @@ sales = [
     path("sales/add/", sales_add, name="sales_add"),
 ]
 
+
+payments = [
+    path("payments/received/", payments_received, name="payments_received"),
+]
+
+
 # adding to the urls
 appname = "sales"
 urlpatterns = []
 urlpatterns += sales
+urlpatterns += payments
