e7a9e4ba3520dd18fd0e36d31725b3c0174f8356
Siddhartha Khanal <sidkhanal77@gmail.com>
2024-08-20 23:15:17 +0545

    overhauled bank account creation and edit

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -4,32 +4,45 @@ from sales.models import PaymentReceived
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.db.models import Sum
+import collections
+
+from django.db.models import Sum, CharField, Value
 
 from icecream import ic
 
+
 @login_required
 def accounts(request):
-    
+
     bank_accounts = BankAccount.objects.filter(tenant=request.tenant)
     cash_accounts = CashAccount.objects.filter(tenant=request.tenant)
 
     bank_balance = bank_accounts.aggregate(balance=Sum("balance"))["balance"] or 0
     cash_balance = cash_accounts.aggregate(balance=Sum("balance"))["balance"] or 0
-    
-    payment_made = PaymentMade.objects.filter(tenant= request.tenant).order_by("created_at")[:5]
-    payment_received = PaymentReceived.objects.filter(tenant = request.tenant).order_by("created_at")[:5]
-    
-    ic(payment_made)
-    ic(payment_received)
-    
-    
+
+    payment_made = PaymentMade.objects.filter(tenant=request.tenant)
+    payment_received = PaymentReceived.objects.filter(tenant=request.tenant)
+
+    recent_transactions = list(
+        payment_made.values("payment_date", "amount", "payment_method").annotate(
+            transaction_type=Value("made", output_field=CharField())
+        )[:2]
+    ) + list(
+        payment_received.values("payment_date", "amount", "payment_method").annotate(
+            transaction_type=Value("received", output_field=CharField()),
+        )[:2]
+    )
+    sorted_transaction = sorted(
+        recent_transactions, key=lambda x: x["payment_date"], reverse=True
+    )
+
     context = {
         "bank_accounts": bank_accounts,
         "cash_accounts": cash_accounts,
         "total_balance": cash_balance + bank_balance,
         "bank_balance": bank_balance,
         "cash_balance": cash_balance,
+        "recent_transactions": sorted_transaction,
     }
 
     return render(
